ffc556e6e0f3c46ce5ce7d2731e918e1ed60bcbb
dbessa <danielbessa01@gmail.com>
2023-10-13 17:30:10 -0300

    new version for lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 22:39:34 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/08 18:00:33 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/13 17:29:04 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,12 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	t_list	*save;
+	t_list	*new;
 
-	if (!lst || !f || !del)
-		return (0);
-	new_list = ft_lstnew(f(lst->content));
-	if (!new_list)
-		return (0);
-	save = new_list;
-	lst = lst->next;
-	while (lst)
-	{
-		new_list->next = ft_lstnew(f(lst->content));
-		if (!new_list->next)
-		{
-			ft_lstclear(&save, del);
-			return (0);
-		}
-		new_list = new_list->next;
-		lst = lst->next;
-	}
-	new_list->next = NULL;
-	return (save);
+	if (lst == NULL)
+		return (NULL);
+	new = f(lst);
+	if (lst->next != NULL)
+		new->next = ft_lstmap(lst->next, f, del);
+	return (new);
 }
