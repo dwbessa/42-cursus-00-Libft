3974a056bcb863a8945d1b019760a41ea6a1f688
dwbessa <danielbessa01@gmail.com>
2023-10-14 11:56:41 -0300

    adjusting lstadd_back with a temp pointer 'cause i have to make a swap to the original lst

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 11:46:18 by dbessa            #+#    #+#             */
-/*   Updated: 2023/09/11 12:03:23 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/14 11:55:21 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list	*temp;
+
 	if (!*lst)
 	{
 		*lst = new;
 		return ;
 	}
-	while ((*lst)->next != NULL)
-		*lst = (*lst)->next;
-	(*lst)->next = new;
+	temp = *lst;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new;
 }
