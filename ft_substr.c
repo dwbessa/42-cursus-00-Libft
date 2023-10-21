782b10b0f8179fcf94143c78f3f9af4b69fce62d
dwbessa <danielbessa01@gmail.com>
2023-10-20 23:37:47 -0300

    new substr
    thats it

--- ft_substr.c
+++ ft_substr.c
@@ -6,26 +6,34 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 17:24:59 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/17 15:00:30 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/10/20 23:37:10 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
+
 {
-	char	*final_str;
 	size_t	i;
+	size_t	len_src;
+	char	*destiny;
 
 	i = 0;
-	final_str = malloc(sizeof(char) * (len + 1));
-	if (!final_str)
+	len_src = ft_strlen(s);
+	if (start > len_src)
+		return (ft_strdup(""));
+	else if (start + len > len_src)
+		len = len_src - start;
+	destiny = ft_calloc(len + 1, sizeof(char));
+	if ((!destiny))
 		return (NULL);
-	while (i < len)
+	while (s[start] && i < len)
 	{
-		final_str[i] = s[start + i];
+		destiny[i] = s[start];
 		i++;
+		start++;
 	}
-	final_str[i] = '\0';
-	return (final_str);
+	destiny[i] = '\0';
+	return (destiny);
 }
