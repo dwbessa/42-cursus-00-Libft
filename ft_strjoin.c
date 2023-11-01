66dd634d798797f4ce63c80d2a81ffd269742928
dwbessa <danielbessa01@gmail.com>
2023-11-01 11:41:30 -0300

    fix: norminette

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 10:54:47 by dbessa            #+#    #+#             */
-/*   Updated: 2023/10/24 19:04:24 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/11/01 11:40:44 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,27 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
 	size_t	count1;
 	size_t	count2;
 	char	*s3;
 
 	if (!s1 || !s2)
 		return (NULL);
-	i = ft_strlen(s1);
-	j = ft_strlen(s2);
 	count1 = 0;
 	count2 = 0;
-	s3 = malloc(sizeof(char) * (i + j + 1));
+	s3 = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!s3)
 		return (NULL);
-	while (count1 != i)
+	while (count1 != ft_strlen(s1))
 	{
 		s3[count1] = s1[count1];
 		count1++;
 	}
-	while (count2 != j)
+	while (count2 != ft_strlen(s2))
 	{
 		s3[count1 + count2] = s2[count2];
 		count2++;
 	}
-	s3[i + j] = '\0';
+	s3[count1 + count2] = '\0';
 	return (s3);
 }
