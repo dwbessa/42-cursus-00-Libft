56ca67286455aa05ece64d87ed672709621236ca
dwbessa <danielbessa01@gmail.com>
2023-11-17 20:11:36 -0300

    refactor: adjusting protectiong malloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: dbessa <dbessa@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 18:19:02 by dbessa            #+#    #+#             */
-/*   Updated: 2023/09/15 22:16:06 by dbessa           ###   ########.fr       */
+/*   Updated: 2023/11/17 20:11:14 by dbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	c = 0;
 	total_size = nmemb * size;
 	ptr = malloc(total_size);
-	if (ptr != NULL)
-		ft_memset(ptr, c, total_size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, c, total_size);
 	return (ptr);
 }
